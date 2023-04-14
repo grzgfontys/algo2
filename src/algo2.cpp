d95dd4f161a4113de97923c840f0640aefe84070
Jakub Pawlak <234767@edu.p.lodz.pl>
2023-04-14 21:06:42 +0200

    Removed unnecessary stuff

--- src/algo2.cpp
+++ src/algo2.cpp
@@ -1,10 +1,5 @@
-﻿// algo2.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
-#include <stdlib.h> 
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
-#include <time.h> 
 #include <unordered_map>
 #include <queue>
 #include <algorithm>
@@ -241,14 +236,3 @@ int main()
 
     return 0;
 }
-
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
