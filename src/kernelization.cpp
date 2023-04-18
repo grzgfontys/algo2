00eddb6b2d39d21c1cdec98df64cd8f6c9cea921
Jakub Pawlak <234767@edu.p.lodz.pl>
2023-04-18 14:26:17 +0200

    Added function to add pendant vertices

--- src/kernelization.cpp
+++ src/kernelization.cpp
@@ -1,6 +1,7 @@
 #include "kernelization.h"
 #include "graph.h"
 #include <ranges>
+#include <stdexcept>
 
 namespace views = std::views;
 
@@ -29,4 +30,30 @@ std::vector<int> pendant_vertices(const Graph& graph) {
 std::vector<int> tops_vertices(const Graph& graph, int edge_count) {
     auto tops_vertices = graph.vertices() | views::filter(tops_checker(graph, edge_count));
     return {tops_vertices.begin(), tops_vertices.end()};
+}
+
+void add_pendants(Graph& graph, int count) {
+    auto is_pendant = pendant_checker(graph);
+    auto is_not_pendant = [&](int v) { return !is_pendant(v); };
+
+    auto non_pendant_vertices = graph.vertices() | views::filter(is_not_pendant);
+
+    for (auto vertex: non_pendant_vertices) {
+        if (count == 0) {
+            break;
+        }
+        if (graph.degree(vertex) > 1) {
+            auto adjacent = graph.adjacent(vertex);
+            if (!std::ranges::all_of(adjacent.begin(), adjacent.end(), is_not_pendant)) {
+                continue;
+            }
+            for (int adjacent_vertex: adjacent | views::drop(1)) {
+                graph.remove_edge({vertex, adjacent_vertex});
+            }
+            count--;
+        }
+    }
+    if (count > 0) {
+        throw std::runtime_error("Cannot add any more pendants");
+    }
 }
