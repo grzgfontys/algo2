513482aa0856717e443d9309407d5667830b19cf
Jakub Pawlak <234767@edu.p.lodz.pl>
2023-04-18 14:18:06 +0200

    Improved readability

--- src/kernelization.cpp
+++ src/kernelization.cpp
@@ -1,7 +1,8 @@
 #include "kernelization.h"
 #include "graph.h"
 #include <ranges>
-#include <stdexcept>
+
+namespace views = std::views;
 
 auto isolation_checker(const Graph& graph) {
     return [&](int vertex) { return graph.degree(vertex) == 0; };
@@ -16,16 +17,16 @@ auto tops_checker(const Graph& graph, int edge_count) {
 }
 
 std::vector<int> isolated_vertices(const Graph& graph) {
-    auto isolated_vertices = graph.vertices() | std::views::filter(isolation_checker(graph));
+    auto isolated_vertices = graph.vertices() | views::filter(isolation_checker(graph));
     return {isolated_vertices.begin(), isolated_vertices.end()};
 }
 
 std::vector<int> pendant_vertices(const Graph& graph) {
-    auto pendant_vertices = graph.vertices() | std::views::filter(pendant_checker(graph));
+    auto pendant_vertices = graph.vertices() | views::filter(pendant_checker(graph));
     return {pendant_vertices.begin(), pendant_vertices.end()};
 }
 
 std::vector<int> tops_vertices(const Graph& graph, int edge_count) {
-    auto tops_vertices = graph.vertices() | std::views::filter(tops_checker(graph,edge_count));
+    auto tops_vertices = graph.vertices() | views::filter(tops_checker(graph, edge_count));
     return {tops_vertices.begin(), tops_vertices.end()};
 }
