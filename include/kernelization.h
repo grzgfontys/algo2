3e385e24977c862a2cea6eae428cb27737caab0a
Jakub Pawlak <234767@edu.p.lodz.pl>
2023-04-19 22:37:23 +0200

    Added functions to the header file

--- include/kernelization.h
+++ include/kernelization.h
@@ -2,6 +2,7 @@
 #define ALGORITHMS_2_KERNELIZATION_H
 
 #include <vector>
+
 class Graph;
 
 std::vector<int> isolated_vertices(const Graph& graph);
@@ -9,5 +10,13 @@ std::vector<int> isolated_vertices(const Graph& graph);
 std::vector<int> pendant_vertices(const Graph& graph);
 
 std::vector<int> tops_vertices(const Graph& graph, int edge_count);
+
+void add_pendants(Graph& graph, unsigned int count);
+
+void remove_pendants(Graph& graph, unsigned int count);
+
+void add_tops(Graph& graph, int edge_count, unsigned int count);
+
+void remove_tops(Graph& graph, int edge_count, unsigned int count);
 
 #endif //ALGORITHMS_2_KERNELIZATION_H
